5b18db96cacaed4572fa2f863e29e2a7667cdcf2
Diego <34724728+Calcoph@users.noreply.github.com>
2024-03-12 16:10:17 +0100

    auto enable openvpn@server

--- vpn/install.sh
+++ vpn/install.sh
@@ -10,4 +10,6 @@ sudo cp openvpn-ca/pki/issued/server.crt /etc/openvpn
 sudo cp openvpn-ca/pki/private/server.key /etc/openvpn
 sudo cp server.conf /etc/openvpn/server.conf
 sudo cp sysctl.conf /etc/sysctl.conf
+sudo systemctl enable openvpn@server
+sudo systemctl start openvpn@server
 sysctl -p
